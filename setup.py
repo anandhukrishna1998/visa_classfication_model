591e0836917454b8feabd6574f7884261d70c341
anandhukrishna1998 <63602359+anandhukrishna1998@users.noreply.github.com>
2024-11-22 18:34:30 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Anandhu",
+    author_email="anandhukrishna091@gmail.com",
+    packages=find_packages()
+)
